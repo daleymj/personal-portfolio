d4cbfbd784b89bb5a65b558fcd08a767325a505c
daleymj <daleymj87@gmail.com>
2017-03-24 12:13:27 -0700

    gettin there

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
+	<link href="https://fonts.googleapis.com/css?family=Passion+One|Roboto" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
 
 	<?php wp_head(); //hook. REQUIRED for plugins and admin bar to work ?>
 </head>
