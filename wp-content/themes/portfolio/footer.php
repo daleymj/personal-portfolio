22235a61e447612b863272d63a15ecdf69911bff
daleymj <daleymj87@gmail.com>
2017-03-31 12:23:34 -0700

    soon

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -2,7 +2,7 @@
 <!-- end wrapper -->
 
 <footer id="footer" role="contentinfo">
-  <small>&copy; 2017 by <?php bloginfo('name'); ?>. All Rights Reserved.</small>
+  <small>&copy; 2017 <?php bloginfo('name'); ?>. All Rights Reserved.</small>
 </footer>
 
 <?php wp_footer(); //HOOK. required for admin bar & plugins to work ?>
